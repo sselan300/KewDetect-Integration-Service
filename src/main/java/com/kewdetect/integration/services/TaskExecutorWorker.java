6f151e42b7d4330a0e23bd87bf5b37930f7211c2
long tran <tqlong.net@gmail.com>
2021-10-20 08:27:05 +0700

    Log to File

--- src/main/java/com/kewdetect/integration/services/TaskExecutorWorker.java
+++ src/main/java/com/kewdetect/integration/services/TaskExecutorWorker.java
@@ -1,24 +1,33 @@
 package com.kewdetect.integration.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.kwm.common.analytics.Analytics;
+import com.kwm.common.analytics.AnalyticsHelper;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TaskExecutorWorker {
 
-    private static Logger log = LoggerFactory.getLogger(TaskExecutorWorker.class);
+    private Analytics analytics = AnalyticsHelper.geAnalytics(TaskExecutorWorker.class);
 
-    public TaskExecutorWorker(String identityNo){
+    public TaskExecutorWorker(String identityNo) {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2, //  corePoolSize
                 2, //  maximumPoolSize
                 2, //
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(2));
-        executor.execute(() -> log.info(identityNo + " :identityNo Thread named : " + Thread.currentThread().getName()));
-        executor.execute(() -> log.info(identityNo + " :identityNo Thread named : " + Thread.currentThread().getName()));
+        executor.execute(() -> analytics.logEvent(identityNo + " :identityNo Thread named : " + Thread.currentThread().getName()));
+        executor.execute(() -> analytics.logEvent(identityNo + " :identityNo Thread named : " + Thread.currentThread().getName()));
         executor.shutdown();
+
+        analytics.trace("This is TRACE");
+        analytics.debug("This is DEBUG");
+        analytics.info("This is INFO");
+        analytics.warn("This is WARN");
+        analytics.error("This is ERROR");
     }
 }
